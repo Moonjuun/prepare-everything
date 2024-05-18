89991da53c53c62c94bec336af2fd2581531e31b
mj94 <mj94@vntgcorp.com>
2024-05-18 23:14:59 +0900

    MOD: 메타 데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,25 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Kuri, 이 중에 하나쯤은???",
   description: "Kuri, 쿠리, 룰렛, 계산기, 사다리 게임, 게시판, 심리 테스트",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "쿠리 Kuri, 이 중에 하나쯤은??? ",
+    url: "https://www.ai-ai.link",
+    siteName: "쿠리 Kuri",
+    locale: "ko",
+    type: "website",
+  },
+  keywords: [
+    "쿠리 Kuri",
+    "계산기",
+    "룰렛",
+    "유용한 정보",
+    "심리 테스트",
+    "사다리 게임",
+    "게시판",
+  ],
 };
 
 export default function RootLayout({
