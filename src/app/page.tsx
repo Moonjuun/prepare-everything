bcc69476baadc4255be6e9234781a3c33f8e9913
mj94 <mj94@vntgcorp.com>
2024-05-18 21:47:27 +0900

    Del : getLPTheme 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ import AppAppBar from "@/common/AppAppBar";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import getLPTheme from "@/common/getLPTheme";
+
 import Banner from "@/common/Banner";
+import MainRoulette from "./play/roulette/page";
 
 export default function Home() {
   const [mode, setMode] = React.useState<PaletteMode>("light");
-  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
-  const LPtheme = createTheme(getLPTheme(mode));
+
   const defaultTheme = createTheme({ palette: { mode } });
 
   const toggleColorMode = () => {
@@ -22,11 +22,13 @@ export default function Home() {
 
   return (
     <>
-      <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
+      <ThemeProvider theme={defaultTheme}>
         <CssBaseline />
         <Box sx={{ bgcolor: "background.default" }}>
           <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
-          <main className={styles.main}></main>
+          <main className={styles.main}>
+            <MainRoulette />
+          </main>
           <Footer2 />
         </Box>
       </ThemeProvider>
