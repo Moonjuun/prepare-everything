6b809d234fbfe2733ea9f2a072f01e32d6303ca0
mj94 <mj94@vntgcorp.com>
2024-05-18 22:47:22 +0900

    ADD : 레이아웃 섹션 3개로 분리

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,16 +1,14 @@
-// ClientLayout.tsx
 "use client";
 
 import * as React from "react";
-import { PaletteMode } from "@mui/material";
+import { PaletteMode, Grid } from "@mui/material";
 import Footer2 from "@/common/Footer2";
 import AppAppBar from "@/common/AppAppBar";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 import styles from "./page.module.css";
-import Banner from "@/common/Banner";
-import MainRoulette from "./play/roullete/page";
 
 export default function ClientLayout({
   children,
@@ -30,7 +28,18 @@ export default function ClientLayout({
       <CssBaseline />
       <Box sx={{ bgcolor: "background.default" }}>
         <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
-        <main className={styles.main}>{children}</main>
+        <Grid container className={styles.main}>
+          <Grid item maxWidth={"20%"} className={styles.leftsection}>
+            Left Section
+          </Grid>
+          <Grid item maxWidth={"60%"}>
+            {children}
+          </Grid>
+          <Grid item maxWidth={"20%"}>
+            Right Section
+          </Grid>
+        </Grid>
+
         <Footer2 />
       </Box>
     </ThemeProvider>
