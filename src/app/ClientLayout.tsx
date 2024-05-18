8fb9aaa4bd8d9d53b69d201d89d0ac8c69aa3dd1
mj94 <mj94@vntgcorp.com>
2024-05-19 00:35:56 +0900

    MOD : 다크모드 로컬스토리지 처리

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,37 +1,36 @@
 "use client";
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { PaletteMode, Grid } from "@mui/material";
 import Footer2 from "@/common/Footer2";
 import AppAppBar from "@/common/AppAppBar";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-
 import styles from "./page.module.css";
+
 export default function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const isClient = typeof window !== "undefined"; // 클라이언트 측에서 실행되는지 확인
-
-  // 로컬 스토리지에서 이전에 선택한 모드 불러오기
-  const savedMode = isClient ? localStorage.getItem("colorMode") : null;
-  const [mode, setMode] = useState<PaletteMode>(
-    savedMode === "dark" ? "dark" : "light"
-  );
+  const [mode, setMode] = useState<PaletteMode>("light");
 
   const defaultTheme = createTheme({ palette: { mode } });
 
   const toggleColorMode = () => {
-    if (isClient) {
-      // 클라이언트 측에서 실행 중일 때만 로컬 스토리지 사용
-      const newMode = mode === "dark" ? "light" : "dark";
-      setMode(newMode);
-      localStorage.setItem("colorMode", newMode);
-    }
+    // 토글 후 현재 모드를 로컬 스토리지에 저장
+    const newMode = mode === "dark" ? "light" : "dark";
+    setMode(newMode);
+    localStorage.setItem("colorMode", newMode);
   };
+
+  useEffect(() => {
+    // 로컬 스토리지에서 이전에 선택한 모드 불러오기
+    const savedMode = localStorage.getItem("colorMode");
+    if (savedMode === "dark" || savedMode === "light") {
+      setMode(savedMode);
+    }
+  }, []);
 
   return (
     <ThemeProvider theme={defaultTheme}>
